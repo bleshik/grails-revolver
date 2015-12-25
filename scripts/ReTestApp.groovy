62eacdad736db87e57617397cbbc2f7a18490d76
Alexey Balchunas <bleshik@gmail.com>
2015-12-25 11:53:11 +0700

    ReTestApp Script: code cleanup

--- scripts/ReTestApp.groovy
+++ scripts/ReTestApp.groovy
@@ -1,20 +1,3 @@
-/*
- * Copyright 2004-2005 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
 import grails.test.runtime.GrailsApplicationTestPlugin
 import grails.test.runtime.TestRuntime
 import grails.test.runtime.TestRuntimeFactory
@@ -24,21 +7,11 @@ import org.codehaus.groovy.grails.test.runner.phase.IntegrationTestPhaseConfigur
 import grails.util.Holders
 import org.grails.datastore.mapping.simple.SimpleMapDatastore
 
-/**
- * Gant script that runs the Grails unit tests
- *
- * @author Graeme Rocher
- * @author Luke Daley
- * @since 0.4
- */
 includeTargets << grailsScript("_GrailsClean")
 includeTargets << grailsScript("_GrailsTest")
 
 TEST_PHASE_AND_TYPE_SEPARATOR = projectTestRunner.TEST_PHASE_AND_TYPE_SEPARATOR
 
-/**
- * Initialises a persistence context and bootstraps the application.
- */
 integrationPhaseConfigurer = new IntegrationTestPhaseConfigurer(projectTestRunner.projectTestCompiler, projectLoader) {
 
     @Override
@@ -59,6 +32,7 @@ integrationPhaseConfigurer = new IntegrationTestPhaseConfigurer(projectTestRunne
     }
 
 }
+
 projectTestRunner.testFeatureDiscovery.configurers.integration = integrationPhaseConfigurer
 
 integrationTestPhaseCleanUp = {
